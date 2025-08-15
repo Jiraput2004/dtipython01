16928dc32388758e2e53c9ea0d0ce6bf405bbfe9
Jiraput2004 <jiraput.nayam.non.github@gmail.com>
2025-08-15 16:12:26 +0700

    V.2: Code ws 1-4 but not complete

---
+++ workshop03.py
@@ -0,0 +1,18 @@
+#คำสังแสดงผลข้อมูล ที่หน้าจอ ในฟังชัน print()
+# string ข้อความ 
+print('Hello')
+print("Hi")
+print('''Hey''')
+print("""Hum""")
+# ตัวเลข
+print(123456)   # integar/int
+print(12.5456)   #float
+
+#boolean ตรรฌะ มี/ตัวเลข ค่า True / False
+print(True)
+print(False)
+
+# expresslon นิพจน์ คือ อะไรตามที่มี protess การที่ทำงาน
+print(10 + 20)
+print(50 > 60)
+print('Bangna' <= 'Bangpoo')
